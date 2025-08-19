f91b6b2e3df6b7f146cfc5d6c12d4ad31cb1658c
denisradadiya9807 <denisradadiya10@gmail.com>
2025-08-19 16:08:03 +0530

    change mongoconnection

--- utility/connection.js
+++ utility/connection.js
@@ -1,3 +1,17 @@
-let mongoose = require('mongoose');
-let mongoconnection = mongoose.createConnection(process.env.MONGO_URI);
+// let mongoose = require('mongoose');
+// let mongoconnection = mongoose.createConnection(process.env.MONGO_URI);
+// module.exports = mongoconnection;
+
+const mongoose = require("mongoose");
+
+const mongoconnection = process.env.MONGO_URI;
+
+mongoose.connect(mongoconnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("✅ MongoDB Connected"))
+    .catch(err => console.error("❌ MongoDB Connection Error:", err));
 module.exports = mongoconnection;
+
+
